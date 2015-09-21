e64ef0ac59d971b6c2997caf31cacc847ea67411
unknown <GEVI@LT-778790.statoil.net>
2015-09-21 14:13:51 +0200

    Addded store +input field

--- routes/store.js
+++ routes/store.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('store', { title: 'Express under store ' });
 });
 
+router.route('/message')
+		.post(function(req,res,next) {
+			console.log('Receiving message ' + req.body.message);
+			res.send('Message was ' + req.body.message);
+		});
+
 module.exports = router;
