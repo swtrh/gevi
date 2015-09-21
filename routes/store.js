aee7cbe7db950d7effc5906045c6878c34ff5215
unknown <GEVI@LT-778790.statoil.net>
2015-09-21 14:56:03 +0200

    Addded store and last file

--- routes/store.js
+++ routes/store.js
@@ -1,9 +1,10 @@
+//var NODE_ENV=development(local) | production (azure default)
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('store', { title: 'Express under store ' });
+  res.render('store', { title: 'Express under store ', messsageURL: process.env.MESSAGE_URL });
 });
 
 router.route('/message')
