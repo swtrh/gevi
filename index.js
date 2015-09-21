93d226f608a287e166f9091a3b2c019847098e81
unknown <GEVI@LT-778790.statoil.net>
2015-09-21 11:36:59 +0200

    fixed port logging

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
+var port = (process.env.PORT || '3000')
 
 app.get('/', function (req,res) {
 	res.send('Hello world')
 });
 
-console.log('Server is listening on port 3000')
+app.listen(port);
 
-app.listen(3000);
+console.log('Server is listening on port 3000')
